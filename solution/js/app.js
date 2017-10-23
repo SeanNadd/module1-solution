384c71f1aeee3351214055053c6b3176dcfdd856
Juan Bernardo Pollacchi <seannadd@gmail.com>
2017-10-23 16:28:12 +0200

    Solved

---
+++ solution/js/app.js
@@ -0,0 +1,32 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.checkLunch = function () {
+      if($scope.meals != "" && $scope.meals != undefined){
+        var mealsArray = $scope.meals.split(',');
+        var counter = 0;
+       for (var i = 0; i < mealsArray.length; i++) {
+          if(mealsArray[i].trim() != ""){
+            counter++;
+          }
+        }
+        if(counter <= 3){
+          $scope.messageType = "success";
+          $scope.message = "Enjoy!";
+        }else{
+          $scope.message = "Too much";
+        }
+      }else{
+        $scope.messageType = "fail";
+        $scope.message = "Please enter data first"
+      }
+    };
+    
+  }
+
+})();
